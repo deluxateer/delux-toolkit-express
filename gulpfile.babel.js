8a179eb2f565a6f616beaa16d656ea198901932b
Deluxateer <deluxateer@gmail.com>
2019-06-11 13:34:23 -0500

    adds plugin to extract css stylesheet from webpack bundling

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,6 @@
 import {src, dest, series, parallel, watch} from 'gulp';
 import webpack from 'webpack-stream';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 // import {test} from './tasks/test';
 
@@ -11,30 +12,41 @@ const test = () => (
 
 exports.test = test;
 
-// const scss = () => (
-//   src('src/scss/index.scss')
-//     .pipe(webpack({
-//       module: {
-//         rules: [{
-//           test: /\.scss$/,
-//           use: [
-//             {
-//               loader: 'style-loader'
-//             },
-//             {
-//               loader: 'css-loader'
-//             },
-//             {
-//               loader: 'sass-loader',
-//               options: {
-//                 outputStyle: 'compressed'
-//               }
-//             }
-//           ]
-//         }]
-//       }
-//     }))
-//     .pipe(dest('dist/'))
-// );
+const scss = () => (
+  src('src/scss/index.scss')
+    .pipe(webpack({
+      output: {
+        // residual js file that should be deleted
+        // finding a way to avoid this altogether would be awesome
+        filename: 'garbage.js' 
+      },
+      module: {
+        rules: [{
+          test: /\.scss$/,
+          use: [
+            {
+              // loader: 'style-loader'
+              loader: MiniCssExtractPlugin.loader
+            },
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                outputStyle: 'compressed'
+              }
+            }
+          ]
+        }]
+      },
+      plugins: [
+        new MiniCssExtractPlugin({
+          filename: 'styles.css'
+        })
+      ]
+    }))
+    .pipe(dest('dist/'))
+);
 
-// exports.scss = scss;
+exports.scss = scss;
