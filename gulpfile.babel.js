852df1050f4c374d1ee7505aed1a0e6bfa9d7b1e
Deluxateer <deluxateer@gmail.com>
2019-06-11 23:45:41 -0500

    adds general style task with gulp instead of webpack

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,81 +1,56 @@
 import path from 'path';
 import {src, dest, series, parallel, watch} from 'gulp';
-import del from 'del';
 import webpack from 'webpack-stream';
+const browserSync = require('browser-sync').create();
+import sourcemaps from 'gulp-sourcemaps';
+import sass from 'gulp-sass';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import rename from 'gulp-rename';
 
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+const styles = () => {
+  const postCssPlugins = [
+    autoprefixer()
+  ];
+  return (
+    // src('src/scss/index.scss')
+    src(path.resolve(__dirname, "src", "scss", "index.scss"))
+    .pipe(sourcemaps.init({ loadMaps: true, largeFile: true }))
+    .pipe(sass({
+      // outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(postcss(postCssPlugins))
+    .pipe(rename("styles.min.css"))
+    .pipe(sourcemaps.write())
+    .pipe(dest(path.resolve(__dirname, "dist", "css")))
+  );
+};
 
-// import {test} from './tasks/test';
-
-const views = () => (
-  // src('src/views/index.html')
-  //   .pipe(webpack({
-  //     entry: './js/index.js',
-  //     output: {
-  //       path: path.resolve(__dirname, 'dist'),
-  //       filename: 'garbage.js',
-  //     },
-  //     context: path.resolve(__dirname, './src'),
-  //     mode: 'development',
-  //     // module: {
-  //     //   rules: [{
-  //     //     test: /\.html$/,
-  //     //     loader: 'babel-loader',
-  //     //   }]
-  //     // },
-  //     plugins: [
-  //       new HtmlWebpackPlugin({
-  //         template: './views/index.html'
-  //       })
-  //     ]
-  //   }))
-);
-
-const test = () => (
+const js = () => (
   src('src/js/index.js')
-    .pipe(webpack(require('./tasks/webpack.config')))
-    .pipe(dest('dist/'))
-);
-
-exports.test = test;
-
-const scss = () => (
-  src('src/scss/index.scss')
     .pipe(webpack({
-      output: {
-        // residual js file that should be deleted
-        // finding a way to avoid this altogether would be awesome
-        filename: 'garbage.js' 
+      context: path.resolve(__dirname, './src'),
+      entry: {
+        app: './js/index.js'
       },
+      output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+      },
+      mode: 'development',
       module: {
         rules: [{
-          test: /\.scss$/,
-          use: [
-            {
-              // loader: 'style-loader'
-              loader: MiniCssExtractPlugin.loader
-            },
-            {
-              loader: 'css-loader'
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                outputStyle: 'compressed'
-              }
-            }
-          ]
+          test: /\.js$/,
+          exclude: '/node_modules',
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/preset-env']
+          }
         }]
-      },
-      plugins: [
-        new MiniCssExtractPlugin({
-          filename: 'styles.css'
-        })
-      ]
+      }
     }))
     .pipe(dest('dist/'))
 );
 
-exports.views = views;
-exports.scss = scss;
+exports.styles = styles;
+exports.js = js;
--- tasks/test.js
+++
@@ -1,5 +0,0 @@
-export const test = () => (
-  src('src/js/**/*.js')
-    .pipe(webpack(require('./webpack.config')))
-    .pipe(dest('dist/'))
-);
