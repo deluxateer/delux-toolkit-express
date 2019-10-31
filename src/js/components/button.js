8d73a326ca047bc05180678770f196e6a9f996e2
Deluxateer <deluxateer@gmail.com>
2019-10-31 16:24:53 -0500

    fix back-to-top-btn bug (hidden on fast scroll)

--- src/js/components/button.js
+++ src/js/components/button.js
@@ -1,22 +1,21 @@
 const backToTopBtn = document.querySelector('.back-to-top-btn');
 
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset > 300) {
-    if (!backToTopBtn.classList.contains('btn-enter')) {
-      backToTopBtn.classList.add('btn-enter');
-      backToTopBtn.classList.remove('btn-exit');
-      backToTopBtn.style.display = 'inline-block';
-    }
-  } else if (!backToTopBtn.classList.contains('btn-exit')) {
-    backToTopBtn.classList.add('btn-exit');
-    backToTopBtn.classList.remove('btn-enter');
-    setTimeout(() => {
-      backToTopBtn.style.display = 'none';
-    }, 300);
-  }
-});
-
 if (backToTopBtn) {
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 300) {
+      if (!backToTopBtn.classList.contains('btn-enter')) {
+        backToTopBtn.classList.add('btn-enter');
+        backToTopBtn.classList.remove('btn-exit');
+        if (backToTopBtn.style.display) {
+          backToTopBtn.removeAttribute('style');
+        }
+      }
+    } else if (!backToTopBtn.classList.contains('btn-exit')) {
+      backToTopBtn.classList.add('btn-exit');
+      backToTopBtn.classList.remove('btn-enter');
+    }
+  });
+
   backToTopBtn.addEventListener('click', () => {
     window.scrollTo(0, 0);
   });
